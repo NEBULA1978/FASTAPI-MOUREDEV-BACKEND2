eb564f962dd279ce160e89cf44f83b58a83e0a93
NEBULA1978 <rpascualey@gmail.com>
2023-07-01 21:14:50 +0200

    Primer api con dos rutas basica

--- backend/FasAPI/main.py
+++ backend/FasAPI/main.py
@@ -8,3 +8,8 @@ app = FastAPI()
 # Siempre que llamemos al servidor asimcronamente
 async def root():
     return "Hello World"
+
+@app.get("/url")
+# Siempre que llamemos al servidor asimcronamente
+async def url():
+    return {"url_curso":"https://mouredev.com/python3"}
