90dabbf7b57b1af9ca5e4113fac6b628009cd936
NEBULA1978 <rpascualey@gmail.com>
2023-07-07 12:18:43 +0200

    Creamos 2  ususarios en archivo basic_auth_users.py paso5 auth

--- backend/FasAPI/routers/basic_auth_users.py
+++ backend/FasAPI/routers/basic_auth_users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends,HTTPException
+from fastapi import FastAPI, Depends, HTTPException,status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
@@ -43,20 +43,29 @@ def search_user(username: str):
         return UserDB(users_db[username])
 
 
+async def current_user(token: str = Depends(oauth2)):
+    user = search_user(token)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=" Credenciales de autentificacion invalidas")
+
+
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     # Buscamos usuario en la base de datos
     user_db = users_db.get(form.username)
     if not user_db:
-        raise HTTPException(
-            status_code=400, detail=" El ususario no es correcto")
+        raise HTTPException(status_code=400, detail=" El ususario no es correcto")
     # Comprobamos usuario base de datos
     user = search_user(form.username)
     # Comprobamos contraseña
     if not form.password == user.password:
-            raise HTTPException(
-                status_code=400, detail=" La contraseña no es correcta")
-    return {"acces_token": user.username,"token_type":"bearer"}
+        raise HTTPException(status_code=400, detail=" La contraseña no es correcta")
+    return {"acces_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
 
 
 # Para iniciar api autentificacion:
