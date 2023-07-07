628752f7b822ad9e85dbf94100483d219e21f77b
NEBULA1978 <rpascualey@gmail.com>
2023-07-07 13:32:10 +0200

    Login jwt autentificacion jsonwebtoken 2

--- backend/FasAPI/routers/jwt_auth_users.py
+++ backend/FasAPI/routers/jwt_auth_users.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI, Depends, HTTPException,status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-# from fastapi.security import User
+from jose import jwt
+from passlib.context import CryptContext
+
+
+ALGORITHN = "HS256"
 
 app = FastAPI()
 
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
-
+Crypt = CryptContext(scheme="bcrypt")
 
 class User(BaseModel):
     username: str
@@ -37,3 +41,22 @@ users_db = {
         "password": "654321",
     },
 }
+
+
+def search_user_db(username: str):
+    if username in users_db:
+        return UserDB(**users_db[username])
+    
+
+@app.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    # Buscamos usuario en la base de datos
+    user_db = users_db.get(form.username)
+    if not user_db:
+        raise HTTPException(status_code=400, detail=" El ususario no es correcto")
+    # Comprobamos usuario base de datos
+    user = search_user_db(form.username)
+    # Comprobamos contraseña
+    if not form.password == user.password:
+        raise HTTPException(status_code=400, detail=" La contraseña no es correcta")
+    return {"acces_token": user.username, "token_type": "bearer"}
