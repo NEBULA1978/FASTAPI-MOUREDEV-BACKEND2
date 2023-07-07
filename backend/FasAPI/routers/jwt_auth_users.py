49d3dfafd772cfec772ae320663941ef82f3fc36
NEBULA1978 <rpascualey@gmail.com>
2023-07-07 21:49:21 +0200

    Variante nueva la rama principal error

--- backend/FasAPI/routers/jwt_auth_users.py
+++ backend/FasAPI/routers/jwt_auth_users.py
@@ -31,14 +31,14 @@ users_db = {
         "full_name": "Brais Moure",
         "email": "braismoure@mourede.com",
         "disabled": False,
-        "password": "123456",
+        "password": " $2a$12$Lx7XRVYhX.3fK0nQIrGHUeJK82RtOxQu2.eK1GNXXpsrCSAfZ.CIm ",
     },
     "mouredev2": {
         "username": "mouredev2",
         "full_name": "Brais Moure 2",
         "email": "braismoure2@mourede.com",
         "disabled": True,
-        "password": "654321",
+        "password": " $2a$12$7Yl5xXBT4r9TZFBqpPFOkOIPiYBD9k/EZ9whHmc9ckP1iN3ilpmii ",
     },
 }
 
