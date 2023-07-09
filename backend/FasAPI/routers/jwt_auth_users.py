b0c5d623f1f9e0b8c29deaea48f3f65f4947a5e5
NEBULA1978 <rpascualey@gmail.com>
2023-07-09 13:38:50 +0200

    Parte 19 voy5H29 jwt me falla al añadir current_user

--- backend/FasAPI/routers/jwt_auth_users.py
+++ backend/FasAPI/routers/jwt_auth_users.py
@@ -58,23 +58,29 @@ def search_user_db(username: str):
     if username in users_db:
         return UserDB(**users_db[username])
 
-async def current_user(token: str = Depends(oauth2)):
-    user = search_user(token)
-    
 
-    # Si no encontramos usuario excepcion
+async def auth_user(token: str = Depends(oauth2)):
+
+    user = jwt.decode(token,SECRET,algorithms=ALGORITHMN)
+
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Credenciales de autentificacion invalidas",
-            headers={"www-Authenticate":"Bearer"})
+            headers={"www-Authenticate": "Bearer"},
+        )
+
+
+async def current_user(user: User = Depends(auth_user)):
+    # Si no encontramos usuario excepcion
 
     if user.disabled:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Ususario inactivo")
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Ususario inactivo"
+        )
 
     return user
+
 
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
@@ -95,13 +101,15 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     }
 
     return {
-        "access_token": jwt.encode(access_token,SECRET, algorithm=ALGORITHMN),
+        "access_token": jwt.encode(access_token, SECRET, algorithm=ALGORITHMN),
         "token_type": "bearer",
     }
+
 
 @app.get("/users/me")
 async def me(user: User = Depends(current_user)):
     return user
+
 
 # VOY 5H17
 # PASO3 Final
