4842b58b2ad66b58327848b16fe89be7564daedf
NEBULA1978 <rpascualey@gmail.com>
2023-07-09 13:29:08 +0200

    Parte 18 voy5H24 jwt me falla al añadir current_user

--- backend/FasAPI/routers/jwt_auth_users.py
+++ backend/FasAPI/routers/jwt_auth_users.py
@@ -58,6 +58,23 @@ def search_user_db(username: str):
     if username in users_db:
         return UserDB(**users_db[username])
 
+async def current_user(token: str = Depends(oauth2)):
+    user = search_user(token)
+    
+
+    # Si no encontramos usuario excepcion
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales de autentificacion invalidas",
+            headers={"www-Authenticate":"Bearer"})
+
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Ususario inactivo")
+
+    return user
 
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
@@ -82,6 +99,9 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
         "token_type": "bearer",
     }
 
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
 
 # VOY 5H17
 # PASO3 Final
