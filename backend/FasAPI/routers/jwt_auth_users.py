174cb9fea09cf41470d4dd4bf8471bb591863bf5
NEBULA1978 <rpascualey@gmail.com>
2023-07-09 13:08:33 +0200

    Empezando autentificacion JWT paso 14  arreglado genero access_token elimino variable expire e introduzco dentro de access_token añado en return login access_token

--- backend/FasAPI/routers/jwt_auth_users.py
+++ backend/FasAPI/routers/jwt_auth_users.py
@@ -1,24 +1,21 @@
-from fastapi import FastAPI, Depends, HTTPException,status
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import jwt
 from passlib.context import CryptContext
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
-ALGORITHN = "HS256"
-# Dura 1 minuto
-ACCES_TOKEN_DURATION = 1
+ALGORITHMN = "HS256"
+# Duration is 1 minute
+ACCESS_TOKEN_DURATION = 1
 
 app = FastAPI()
 
 crypt = CryptContext(schemes=["bcrypt"])
 
-# Tenemos endpoint que se llama login
+# We have an endpoint called "login"
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
-# PASO1 hacia arriba
-
-# PASO2 hacia abajo
+# Define user models
 class User(BaseModel):
     username: str
     full_name: str
@@ -30,63 +27,48 @@ class UserDB(User):
     password: str
 
 
-# Una lista o un array
+# User database (dictionary)
 users_db = {
     "mouredev": {
         "username": "mouredev",
         "full_name": "Brais Moure",
         "email": "braismoure@mourede.com",
         "disabled": False,
-        "password": " $2a$12$fcojdAceoKpkkgLBdByvL.s9b.lJYoulzkI4uJjGMwKcsWFHZWjhe ",
+        "password": "$2a$12$fcojdAceoKpkkgLBdByvL.s9b.lJYoulzkI4uJjGMwKcsWFHZWjhe",
     },
     "mouredev2": {
         "username": "mouredev2",
         "full_name": "Brais Moure 2",
         "email": "braismoure2@mourede.com",
         "disabled": True,
-        "password": " $2a$12$MnV0u2VCMOGRqzSl3hmNSOxj8sphooFsqGqdPx7FrX/.BS8Ul8p3y ",
+        "password": "$2a$12$MnV0u2VCMOGRqzSl3hmNSOxj8sphooFsqGqdPx7FrX/.BS8Ul8p3y",
     },
 }
-# Pagina para encriptar contraseñas:
-# https://bcrypt-generator.com/
 
-# PASO2 final
-
-# PASO3 Inicio
-
+# Search user in the database
 def search_user_db(username: str):
     if username in users_db:
         return UserDB(**users_db[username])
-    
 
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
-    # Buscamos usuario en la base de datos
+    # Search user in the database
     user_db = users_db.get(form.username)
     if not user_db:
-        raise HTTPException(status_code=400, detail=" El ususario no es correcto")
-    # Comprobamos usuario base de datos
+        raise HTTPException(status_code=400, detail="El usuario no es correcto")
+
+    # Verify the password hash
     user = search_user_db(form.username)
+    if not crypt.verify(form.password, user.password):
+        raise HTTPException(status_code=400, detail="La contraseña no es correcta")
 
-    # Verificamos contraseña encriptada
-    
-
-    # Comprobamos contraseña
-    if not crypt.verify(form.password,user.password):
-        raise HTTPException(status_code=400, detail=" La contraseña no es correcta")
-    # Tiempo de expiracion
-    # access_token_expiration = timedelta(minutes=ACCES_TOKEN_DURATION)
-
-    # El tiempo de ahora mas 1minuto expira
-
-    # Introduzco abajo y elimino la variable
-    # expire = datetime.utcnow() + timedelta(minutes=ACCES_TOKEN_DURATION)
-
-    access_token={"sub":user.username,
-                "exp":datetime.utcnow() + timedelta(minutes=ACCES_TOKEN_DURATION)} 
+    # Calculate the expiration time for the access token
+    access_token = {
+        "sub": user.username,
+        "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION),
+    }
 
     return {"access_token": access_token, "token_type": "bearer"}
-
 
 
 
