ca89800d4efb2d570150315475c46b9a7c25fc97
NEBULA1978 <rpascualey@gmail.com>
2023-07-09 11:55:40 +0200

    Empezando autentificacion JWT paso 2 en archivo jwt_auth_users.py

--- backend/FasAPI/routers/jwt_auth_users.py
+++ backend/FasAPI/routers/jwt_auth_users.py
@@ -1,3 +1,50 @@
+from fastapi import FastAPI, Depends, HTTPException,status
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+
+app = FastAPI()
+
+# Tenemos endpoint que se llama login
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+
+# PASO1 hacia arriba
+
+# PASO2 hacia abajo
+class User(BaseModel):
+    username: str
+    full_name: str
+    email: str
+    disabled: bool
+
+
+class UserDB(User):
+    password: str
+
+
+# Una lista o un array
+users_db = {
+    "mouredev": {
+        "username": "mouredev",
+        "full_name": "Brais Moure",
+        "email": "braismoure@mourede.com",
+        "disabled": False,
+        "password": "123456",
+    },
+    "mouredev2": {
+        "username": "mouredev2",
+        "full_name": "Brais Moure 2",
+        "email": "braismoure2@mourede.com",
+        "disabled": True,
+        "password": "654321",
+    },
+}
+# PASO2 final
+
+
+# Instalo:
+# pip install "python-jose[cryptography]"
+#  pip install "passlib[bcrypt]"
 
 
 
